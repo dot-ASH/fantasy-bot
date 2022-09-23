5c52eb9f7074ca1a0e602593660e66d07c74b87c
devSASH <sakir.ashker@gmail.com>
2022-09-23 11:17:22 +0600

    trust is heavy word

--- src/commands/sendtxt.js
+++ src/commands/sendtxt.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import { client } from "../../index.js";
-import { AttachmentBuilder, EmbedBuilder } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 config();
 const fantasy_role = process.env.ROLE;
 
@@ -12,7 +12,12 @@ export async function sendtxt(text) {
     const exampleEmbed = new EmbedBuilder()
       .setTitle(`Players,`)
       .setColor(0x5e548e)
-      .setDescription(text);
+      .setDescription(text)
+      .setAuthor({
+        name: "Fantasy League Bot",
+        iconURL: "https://i1.lensdump.com/i/17MRxo.jpg",
+        url: "https://gaming.uefa.com/en/uclfantasy/overview",
+      });
     await Channel.send({
       content: `${fantasy_role}`,
       embeds: [exampleEmbed],
